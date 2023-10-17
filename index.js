e4b2041a6db34fb4ac9380a54005ce70ad77b94d
HafizAbdullah7 <hafizab200@gmail.com>
2023-10-17 00:58:09 -0700

    first commit

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 
+// Configure CORS middleware
+const corsOptions = {
+  origin: '*', // Allow requests from any origin
+  methods: ['GET'], // Allow only GET requests
+  allowedHeaders: ['Content-Type'], // Allow Content-Type header
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
-
+app.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Set Access-Control-Allow-Origin header
+  res.send('Hello, this is the YouTube Video Downloader server!');
+});
 
 app.get('/download', (req, res) => {
   const url = req.query.url;
   ytdl(url, { filter: format => format.container === 'mp4' })
     .pipe(res);
 });
-app.get('/', (req, res) => {
 
-  res.send('Hello, this is the YouTube Video Downloader server!');
-});
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
